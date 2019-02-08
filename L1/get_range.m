6e66daa652d1713fa7e1d522ab1d5345f2b96681
Martin Hsueh <mhsueh2@gmail.com>
2019-02-07 23:15:12 -0500

    Merge conflict resolved

--- L1/get_range.m
+++ L1/get_range.m
@@ -1,20 +1,6 @@
 function [max_x, max_y, min_x, min_y] = get_range(samples)
     % % Get min and max for an array of samples of N*2 matrix
     % % Input: Samples: Array 
-<<<<<<< HEAD
-        [x_col, y_col] = deal(1, 2);
-        [max_x, max_y] = deal(-inf);
-        [min_x, min_y] = deal(inf);
-    
-        for i=1:length(samples)
-            s = samples{i};
-            max_x = max(max_x, max(s(:,x_col)));
-            max_y = max(max_y, max(s(:,y_col)));
-            min_x = min(min_x, min(s(:,x_col)));
-            min_y = min(min_y, min(s(:,y_col)));
-        end
-    end
-=======
     [x_col, y_col] = deal(1, 2);
     [max_x, max_y] = deal(-inf);
     [min_x, min_y] = deal(inf);
@@ -27,4 +13,3 @@ function [max_x, max_y, min_x, min_y] = get_range(samples)
         min_y = min(min_y, min(s(:,y_col)));
     end
 end 
->>>>>>> origin
