b1c5a0d2eead5e990856b32c1d3b83997dd99340
Martin Hsueh <mhsueh2@gmail.com>
2019-02-07 22:58:31 -0500

    Reorgnization ensure better devleopment experience

---
+++ L1/plot_scatter.m
@@ -0,0 +1,10 @@
+function plot_scatter( classes, legends)
+    sz = 5;
+    for i=1:length(classes)
+        x = classes{i}(:,1);
+        y = classes{i}(:,2);
+        scatter(x,y,sz,'filled');
+        hold on;
+    end
+    legend(legends)
+end
