982e30359ab4c44b35452345d6cf1c94dbc2e7e0
Martin Hsueh <mhsueh2@gmail.com>
2019-02-09 16:59:08 -0500

    Add alg to calc average distance

--- L1/plot_1NN_boundry.m
+++ L1/plot_1NN_boundry.m
@@ -1,7 +1,8 @@
 % % A VS B
+k = 1;
 samples = {sample_A, sample_B};
 [X, Y] = create_2d_grid(samples, resolution);
-NN_grid_AB = classify_grid_by_NN(samples, X, Y);
+NN_grid_AB = classify_grid_by_kNN(samples, X, Y, k);
 figure;
 contourf(X, Y, NN_grid_AB);
 hold on;
@@ -11,16 +12,16 @@ hold off;
 % % C VS D VS D
 samples = {sample_C, sample_D, sample_E};
 [X, Y] = create_2d_grid(samples, resolution);
-NN_grid_CDE = classify_grid_by_NN(samples, X, Y);
+NN_grid_CDE = classify_grid_by_kNN(samples, X, Y, k);
 figure;
 contourf(X, Y, NN_grid_CDE);
 hold on;
 plot_scatter(samples, {'Class Boundries', clsC('name'), clsD('name'), clsE('name')})
 hold off;
 
-function  grid_ = classify_grid_by_NN(samples, X, Y)
+function  grid_ = classify_grid_by_kNN(samples, X, Y, k)
 % % Evaluates the samples and classify  points on the grid to get boundry
-% % Input: samples: Array of samples; size: size of grid
+% % Input: samples: Array of samples; meshed X and Y, k number of neighbours
 % % Returns a xy cordinates requried to plot the boundry
     size_ = size(X);
     x_range = size_(1,1);
@@ -35,11 +36,19 @@ function  grid_ = classify_grid_by_NN(samples, X, Y)
         for i=1:length(min_distances)
             samp = samples{i};
             N = length(samp);
+            k_min_d = inf(1, length(k))
             for j=1:N
                 vector = [p;samp(j,:)];
                 d = round(pdist(vector,'euclidean'),5);
-                if d < min_distances(i), min_distances(i) = d; end
+                for i_m=1:k
+                    if d <= k_min_d(i_m)
+                        temp = k_min_d(i_m);
+                        k_min_d(i_m) = d;
+                        d = temp;
+                    end
+                end
             end
+            min_distances(i) = mean(k_min_d)
         end
 
         if length(min_distances) == length(unique(min_distances))
