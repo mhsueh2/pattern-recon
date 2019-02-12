32b0a0e524925ce2763cbf2be6fdb90e196b20a9
Kevin Houlberg <krhoulberg@gmail.com>
2019-02-11 23:06:09 -0500

    fixed legends

--- L1/plot_MED_MICD_MAP.m
+++ L1/plot_MED_MICD_MAP.m
@@ -12,7 +12,8 @@ hold on;
 contour(X, Y, MAP_grid_AB, 'blue');
 plot_error_variance(classes{1}, 10, {'r'});
 plot_error_variance(classes{2}, 10, {'k'});
-plot_scatter(samples, {'MICD Boundries', 'MED Boundries', clsA('name'), clsB('name'), clsA('name'), clsB('name')});
+plot_scatter(samples, {'MICD Boundary', 'MAP Boundary', 'U.S.D. Contour A', 'U.S.D. Contour B', clsA('name'), clsB('name')});
+title('3. CASE 1: MED/MICD/MAP Boundaries');
 hold off;
 
 % % C VS D VS D
@@ -31,5 +32,6 @@ contour(X, Y, MAP_grid_CDE, 'blue');
 plot_error_variance(classes{3}, 10, {'r'});
 plot_error_variance(classes{4}, 10, {'k'});
 plot_error_variance(classes{5}, 10, {'m'});
-plot_scatter(samples, {'MICD Boundries', 'MED Boundries', clsC('name'), clsD('name'), clsE('name'), clsC('name'), clsD('name'), clsE('name')})
+plot_scatter(samples, {'MICD Boundary', 'MAP Boundary', 'U.S.D. Contour C', 'U.S.D. Contour D', 'U.S.D. Contour E', clsC('name'), clsD('name'), clsE('name')});
+title('3. CASE 2: MED/MICD/MAP Boundaries');
 hold off;
--- L1/plot_data_and_variance.m
+++ L1/plot_data_and_variance.m
@@ -1,20 +1,21 @@
 % % 2. Plot samples
 % % % Plot Samples and Unit standard deviation contours
-figure;
-for i=1:length(classes)
-    subplot(length(classes), 1, i);
-    name = classes{i}('name');
-    sample = classes{i}('sample');
-    plot_scatter({sample}, {});
-    plot_error_variance(classes{i}, 10, {'r'});
-    legend(name, 'Covariance Error');
-    hold off;
-end
 
 % % % Plot A vs B
 figure;
-plot_scatter({sample_A,sample_B},{'Class A', 'Class B'})
+plot_error_variance(classes{1}, 10, {'r'});
+hold on;
+plot_error_variance(classes{2}, 10, {'k'});
+plot_scatter({sample_A,sample_B}, {'U.S.D. Contour A', 'U.S.D. Contour B', 'Class A', 'Class B'});
+title('2. CASE 1: Samples & Unit Standard Deviation Contours');
+hold off;
 
 % % % Plot C vs D vs E
 figure;
-plot_scatter({sample_C,sample_D,sample_E},{'Class C', 'Class D', 'Class E'})
+plot_error_variance(classes{3}, 10, {'r'});
+hold on;
+plot_error_variance(classes{4}, 10, {'k'});
+plot_error_variance(classes{5}, 10, {'m'});
+plot_scatter({sample_C,sample_D,sample_E}, {'U.S.D. Contour C', 'U.S.D. Contour D', 'U.S.D. Contour E', 'Class C', 'Class D', 'Class E'}); 
+title('2. CASE 2: Samples & Unit Standard Deviation Contours');
+hold off;
