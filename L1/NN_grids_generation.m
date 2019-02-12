ed6c786d9335caec0757cd6edece717151508014
Murty <31101541+mhsueh2@users.noreply.github.com>
2019-02-11 23:02:26 -0500

    var naming error fixed

--- L1/NN_grids_generation.m
+++ L1/NN_grids_generation.m
@@ -1,9 +1,9 @@
 % % Create grids for Case 1 and 2
 case1_samps = {sample_A, sample_B};
-[X_1, Y_1] = create_2d_grid(samples, resolution);
+[X_1, Y_1] = create_2d_grid(case1_samps, resolution);
 
 case2_samps = {sample_C, sample_D, sample_E};
-[X_2, Y_2] = create_2d_grid(samples, resolution);
+[X_2, Y_2] = create_2d_grid(case2_samps, resolution);
 
 % % Generate classified 1 NN grids
 k = 1;
