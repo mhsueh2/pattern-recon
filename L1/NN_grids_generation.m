1c88d75a22be7fbe284f62bfe87130a11e069058
Murty <31101541+mhsueh2@users.noreply.github.com>
2019-02-11 22:55:53 -0500

    Create NN_grids_generation.m

---
+++ L1/NN_grids_generation.m
@@ -0,0 +1,16 @@
+% % Create grids for Case 1 and 2
+case1_samps = {sample_A, sample_B};
+[X_1, Y_1] = create_2d_grid(samples, resolution);
+
+case2_samps = {sample_C, sample_D, sample_E};
+[X_2, Y_2] = create_2d_grid(samples, resolution);
+
+% % Generate classified 1 NN grids
+k = 1;
+NN_grid_c1 = classify_grid_by_kNN(case1_samps, X_1, Y_1, k);
+NN_grid_c2 = classify_grid_by_kNN(case2_samps, X_2, Y_2, k);
+
+% % Generate classified 5 NN grids
+k = 5;
+five_NN_grid_c1 = classify_grid_by_kNN(case1_samps, X_1, Y_1, k);
+five_NN_grid_c2 = classify_grid_by_kNN(case2_samps, X_2, Y_2, k);
