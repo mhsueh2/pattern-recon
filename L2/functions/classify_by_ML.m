c925189320aa2552057b9421d186e4ed1d833827
Martin Hsueh <mhsueh2@gmail.com>
2019-03-12 12:31:21 -0400

    Simply classify by ML code by utllizing full max() functionality

--- L2/functions/classify_by_ML.m
+++ L2/functions/classify_by_ML.m
@@ -1,14 +1,7 @@
 function grid = classify_by_ML(A, B, C)
     grid = zeros(size(A));
     for i=1:numel(A)
-        max_val = max([A(i) B(i) C(i)]);
-        
-        if max_val == A(i)
-            grid(i) = 1;
-        elseif max_val == B(i)
-            grid(i) = 2;
-        elseif max_val == C(i)
-            grid(i) = 3;
-        end        
+        [max_val, cls_index] = max([A(i) B(i) C(i)]);
+        grid(i) = cls_index;   
     end
 end
